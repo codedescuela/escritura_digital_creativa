efb8ac568fb02b18bbd2edf2de69d3932b2459ae
montoyamoraga <montoyamoraga@gmail.com>
2017-07-19 21:27:16 -0400

    add sinonimizar

---
+++ sesiones/sesion_3/pseudo/sinonimos.py
@@ -0,0 +1,20 @@
+# sinonimizar.py
+# by florencia edwards
+#
+# july 19th 2017
+# v0.0.1
+
+
+# sinonimizar(texto, porcentaje, selector, exportar)
+# texto: text to be transformed
+# porcentaje: percentage of text to be transformed, counted in words
+# selector: 'v' for verbs, 's' for nouns, 'a' for adverbs, etc..
+# exportar: 'si' for exporting to disk, 'no' for just outputting number of possible combinations
+def sinonimizar(texto, porcentaje, selector, exportar):
+    print "TODO"
+
+# function definirGenero
+
+# function definirTiempoVerbal
+
+# function verificarPalabraEspanol
